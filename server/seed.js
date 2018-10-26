b0768a64e7d07762b68d6b6ae30154fd2d0f2567
Kameron Seto <tan@Sad-Panda.local>
2018-10-26 12:29:09 -0700

    create table done

--- server/seed.js
+++ server/seed.js
@@ -2,6 +2,12 @@ const mysql = require('mysql');
 const Promise = require('bluebird');
 const database = 'resy_db';
 
+
+
+
+
+
+
 const conn = mysql.createConnection({
   user: 'student',
   password: 'student',
@@ -13,8 +19,21 @@ db.connectAsync()
   .then(() => db.queryAsync(`CREATE DATABASE IF NOT EXISTS ${database}`))
   .then(() => db.queryAsync(`USE ${database}`))
   .then(() => db.queryAsync(`
+    CREATE TABLE inventory (
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      restaurant_id INT NOT NULL,
+      party INT NOT NULL,
+      avail_at TIMESTAMP NOT NULL,
+      special_seating INT,
+      points INT,
+      quantity INT,
+      FOREIGN KEY inventory_id REFERENCES inventory(id)
+    )
+  `).then(() => db.queryAsync(`
     CREATE TABLE reservations (
       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-      user_id INT NOT NULL
+      user_id INT NOT NULL,
+      inventory_id INT NOT NULL,
+      booked_at TIMESTAMP NOT NULL
     )
-  `)
+  `).then(
