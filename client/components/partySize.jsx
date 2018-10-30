e8c856d063cbfbcb49307be5a48dd648ba0d2452
Kameron Seto <tan@Sad-Panda.local>
2018-10-29 17:12:52 -0700

    App refactored to class component; <li> to PartySize

--- client/components/partySize.jsx
+++ client/components/partySize.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import _ from 'underscore';
 
 const maxPartySize = 20;
+
+const PartySize = () => (
+  <div>
+    <div>Party Size</div>
+    <ul>
+      {_.range(1, maxPartySize + 1).map(i => <li>{i}</li>)}
+    </ul>
+  </div>
+);
 
 export default PartySize;
--- client/index.jsx
+++ client/index.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PartySize from './components/partySize';
-import Date from './components/date';
+import Calendar from './components/calendar';
 import Time from './components/time';
 import Availability from './components/availability';
 import TimesBookedToday from './components/timesBookedToday';
 import TimeslotsLeft from './components/timeslotsLeft';
 import SaveThisRestaurant from './components/saveThisRestaurant';
 
-const App = () => (
-  <div>
-    <div className="main">
-      <div className="header">Make a reservation</div>
-      <div className="body">
-        <PartySize />
-        <Date />
-        <Time />
-        <Availability />
-        <TimesBookedToday />
-        <TimeslotsLeft />
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // the user will have supplied some values from the home page before the restaurant page
+      partySize: 2,
+      date: new Date(),
+      time: '1900',
+    };
+  }
+
+  handleChange() {
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="main">
+          <div className="header">Make a reservation</div>
+          <div className="body">
+            <PartySize partySize={this.state.partySize} handleChange={this.handleChange.bind(this)}/>
+            <Calendar />
+            <Time />
+            <Availability />
+            <TimesBookedToday />
+            <TimeslotsLeft />
+          </div>
+        </div>
+        <SaveThisRestaurant />
       </div>
-    </div>
-    <SaveThisRestaurant />
-  </div>
-);
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
