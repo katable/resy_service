d30428965735bc5e4f643ab754556817a082eb76
Kameron Seto <tan@Sad-Panda.local>
2018-10-29 19:32:11 -0700

    work on the Time component

--- client/components/partySize.jsx
+++ client/components/partySize.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import _ from 'underscore';
+import PropTypes from 'prop-types';
 
 const maxPartySize = 20;
 
-const PartySize = () => (
+const PartySize = ({ partySize, handleChangeParty }) => (
   <div>
     <div>Party Size</div>
-    <ul>
-      {_.range(1, maxPartySize + 1).map(i => <li>{i}</li>)}
-    </ul>
+    <div>For {partySize}</div>
+    <select onChange={handleChangeParty}>
+      {_.range(1, maxPartySize + 1).map(i => <option key={`party${i}`}>{i}</option>)}
+    </select>
   </div>
 );
+
+PartySize.propTypes = {
+  partySize: PropTypes.string.isRequired,
+  handleChangeParty: PropTypes.func.isRequired,
+};
 
 export default PartySize;
--- client/index.jsx
+++ client/index.jsx
@@ -8,31 +8,39 @@ import TimesBookedToday from './components/timesBookedToday';
 import TimeslotsLeft from './components/timeslotsLeft';
 import SaveThisRestaurant from './components/saveThisRestaurant';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       // the user will have supplied some values from the home page before the restaurant page
-      partySize: 2,
+      partySize: '2',
       date: new Date(),
-      time: '1900',
+      time: '12:00 AM',
     };
+
+    this.handleChangeParty = this.handleChangeParty.bind(this);
+    this.handleChangeTime = this.handleChangeTime.bind(this);
   }
 
-  handleChange() {
-
+  handleChangeParty(e) {
+    this.setState({ partySize: e.target.value });
   }
-  
+
+  handleChangeTime(e) {
+    this.setState({ time: e.target.value });
+  }
+
   render() {
+    const { partySize, date } = this.state;
+
     return (
       <div>
         <div className="main">
           <div className="header">Make a reservation</div>
           <div className="body">
-            <PartySize partySize={this.state.partySize} handleChange={this.handleChange.bind(this)}/>
-            <Calendar />
-            <Time />
+            <PartySize partySize={partySize} handleChangeParty={this.handleChangeParty} />
+            <Calendar date={date} />
+            <Time handleChangeTime={this.handleChangeTime} />
             <Availability />
             <TimesBookedToday />
             <TimeslotsLeft />
