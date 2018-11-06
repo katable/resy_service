3ac32e979e8b4f19aca86fcdeecd59f537b99d58
Kameron Seto <tan@Sad-Panda.local>
2018-11-05 17:15:22 -0800

    more polyfill

--- client/components/helper.js
+++ client/components/helper.js
@@ -26,6 +26,18 @@ if (!Object.entries) {
     return resArray;
   };
 }
+
+const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+const concat = Function.bind.call(Function.call, Array.prototype.concat);
+const keys = Reflect.ownKeys;
+
+if (!Object.values) {
+	Object.values = function values(O) {
+		return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+	};
+}
+
 // Polyfill end
 
 
